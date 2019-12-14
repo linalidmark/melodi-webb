50c0898297b5c1005b74f2d252ca231f659b0076
Lina <lina.lidmark@acino.se>
2019-12-15 00:15:40 +0100

    Adds placeholder for score

--- src/Helpers/CellRenderHelpers.js
+++ src/Helpers/CellRenderHelpers.js
@@ -1,5 +1,5 @@
 import React from 'react';
 
 export function ScoreCell() {
-    return <input className="score-cell" type="number" min="1" max="10"></input>
+    return <input className="score-cell" type="number" placeholder="score" min="1" max="10"></input>
 }
